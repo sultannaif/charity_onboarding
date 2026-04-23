68d205db727200942b2afef0de1b9118ca20c4b3
sultannaif <sulltan.naif@gmail.com>
2026-04-23 17:06:00 +0300

    Update __manifest__.py

--- __manifest__.py
+++ __manifest__.py
@@ -1,15 +1,10 @@
 {
     'name': 'Charity Onboarding',
-    'version': '1.5',
-    'summary': 'إدارة مسارات المستفيدين في الجمعية ونموذج التسجيل الذكي',
-    'category': 'Website',
-    'author': 'Sultan Alsalmi',
+    'version': '1.6', # ارفع الإصدار شوي
     'depends': ['base', 'website_slides', 'portal'],
     'data': [
-        'views/templates.xml',
-        'views/partner_view.xml',
+        # 'views/templates.xml',    <-- حط علامة التعليق هذه
+        # 'views/partner_view.xml',  <-- وهذه أيضاً
     ],
     'installable': True,
-    'application': True,
-    'license': 'LGPL-3',
 }
